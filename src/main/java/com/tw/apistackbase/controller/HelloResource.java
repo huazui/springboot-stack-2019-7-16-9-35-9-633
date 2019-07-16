d3fa844ea39eaf2aa7a21989f65cf60973c6a65c
huazui <2509155771@qq.com>
2019-07-17 01:16:06 +0800

    Add files via upload

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,27 +1,123 @@
 package com.tw.apistackbase.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+
+import static java.lang.Integer.parseInt;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/companies")
 public class HelloResource {
+   private final Logger log = Logger.getLogger(this.getClass().getName());
+    private Company company1=new Company(1,"one");
+    private Company company2=new Company(2,"two");
+   private  Company company3=new Company(3,"three");
+   private Company company4=new Company(4,"four");
+   private Company company5=new Company(1,"five");
+   private  Company company6=new Company(1,"six");
+    private Employee employee1=new Employee(1,"zhangsan","male");
+   private  Employee employee2=new Employee(2,"xiaofang","female");
+   private  List<Company> companyList=new ArrayList<Company>();
+   private List<Employee>employeeList=new ArrayList<Employee>();
+   public List<Company>getCompany(){
+       companyList.add(company1);
+       companyList.add(company2);
+       companyList.add(company3);
+       companyList.add(company4);
+       companyList.add(company5);
+       companyList.add(company6);
+       return  companyList;
+   }
 
-    private final Logger log = Logger.getLogger(this.getClass().getName());
-
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
-
-        return ResponseEntity.ok("Hello:" + userName);
+    public List<Employee> getEmployees() {
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        return employeeList;
     }
 
+    @GetMapping()
+public ResponseEntity<List<Company>>getAllCompany(){
+    List<Company> companies=getCompany();
+    return ResponseEntity.ok().body(companies);
+}
+@GetMapping("/{id}")
+public ResponseEntity getACompany(@PathVariable("id") int id) {
+
+    Company result = new Company();
+    List<Company> companies = getCompany();
+    for (int i = 0; i < companies.size(); i++) {
+        if (companies.get(i).getCompanyID() == id) {
+            result = companyList.get(i);
+        }
+    }
+    return ResponseEntity.ok().body(companies);
+}
+    //I can't finish it!!!!!!
+@GetMapping(path = "?page={page}&pageSize={pageSize}")
+
+    public @ResponseBody String getSpecificPageEmployees(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize) {
+
+            List<Company> returnList=getCompany();
+           String result="";
+            for(int i=page;i<=pageSize+page-1;i++){
+              result +=returnList.get(i).getCompanyID()+" "+returnList.get(i).getName()+".";
+            }
+        return result;
+    }
+    //
+@GetMapping("/{id}")
+        public ResponseEntity getEmployees1(@PathVariable("id") int id){
+    Company result = new Company();
+    List<Company> companies = getCompany();
+    List<Employee> employees = getEmployees();
+    for (int i = 0; i < companies.size(); i++) {
+        if (companies.get(i).getCompanyID() == id) {
+            result = companyList.get(i);
+        }
+    }
+    result.setEmployees(employees);
+    List<Employee>getEmployeeOne=result.getEmployees();
+    return ResponseEntity.ok().body(getEmployeeOne);
+}
+    @PostMapping()
+    public ResponseEntity create(@RequestBody Company company){
+        List<Company>companyList=getCompany();
+        companyList.add(company);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity <Company>writenEmployee(@PathVariable("id") int id){
+        List<Company> companies = getCompany();
+        Company result=new Company();
+        for (int i = 0; i < companies.size(); i++) {
+            if (companies.get(i).getCompanyID() == id) {
+                result = companyList.get(i);
+            }
+        }
+        result.setName("new Name");
+        return ResponseEntity.ok().body(result);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") int id) {
+        List<Company> companies = getCompany();
+        List<Employee> employees = getEmployees();
+       companies.get(0).setEmployees(employees);
+       Company result=new Company();
+        for (int i = 0; i < companies.size(); i++) {
+            if (companies.get(i).getCompanyID() == id) {
+                result = companyList.get(i);
+            }
+        }
+        result.setEmployees(null);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
 }
