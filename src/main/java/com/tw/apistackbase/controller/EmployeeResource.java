fe4dab98e54c19e71003e81f9be53755aedc68fa
huazui <2509155771@qq.com>
2019-07-17 00:22:21 +0800

    Add files via upload
    I have make"put"as"delete".......

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -0,0 +1,85 @@
+package com.tw.apistackbase.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.remainderUnsigned;
+
+/**
+ * Created by jxzhong on 18/08/2017.
+ */
+@RestController
+@RequestMapping("/employees")
+public class EmployeeResource {
+    private Employee employee1=new Employee(1,"zhangsan","male");
+    private  Employee employee2=new Employee(2,"xiaofang","female");
+    private List<Employee>employeeList=new ArrayList<Employee>();
+    public List<Employee>getEmployeeList(){
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        return employeeList;
+    }
+    //
+//    @GetMapping(path = "/{userName}", produces = {"application/json"})
+//
+//    public ResponseEntity<String> getAll(@PathVariable String userName) {
+//
+//        return ResponseEntity.ok("Hello:" + userName);
+//    }
+//    @GetMapping()
+//    public  @ResponseBody String getAllCompany() {
+//        List<Employee> employees = getEmployeeList();
+//        String result = "";
+//        for (int i = 0; i < employees.size() - 1; i++) {
+//            result += employees.get(i).getEmployeeID() + " " + employees.get(i).getName() + " " + employees.get(i).getSex() + ",";
+//        }
+//        result += employees.get(employees.size()-1).getEmployeeID() + " " + employees.get(employees.size()-1).getName() + " " + employees.get(employees.size()-1).getSex() + ".";
+//        return result;
+//    }
+//    @GetMapping("/{id}")
+//    public @ResponseBody String getAEmployee(@PathVariable("id") int id) {
+//        List<Employee> employees = getEmployeeList();
+//        String result = "";
+//        for (int i = 0; i < employees.size(); i++) {
+//            if (employees.get(i).getEmployeeID() == id) {
+//                result = employees.get(i).getEmployeeID()+" "+employees.get(i).getName()+" "+employees.get(i).getSex()+".";
+//            }
+//        }
+//        return result;
+//    }
+//    @GetMapping("/{id}")
+//    public @ResponseBody String getEmployeeIndex(@PathVariable("id") String id) {
+//        String array[]=id.split("&");
+//        String arrayPage[]=array[0].split("=");
+//        String arraySize[]=array[1].split("=");
+//        List<Employee>employees=getEmployeeList();
+//        String result="";
+//        for (int i = parseInt(arrayPage[1]);i<parseInt(arraySize[1]);i++){
+//                result += employees.get(i).getEmployeeID() + " " + employees.get(i).getName() + " " + employees.get(i).getSex() + ",";
+//            }
+//            result += employees.get(employees.size()-1).getEmployeeID() + " " + employees.get(employees.size()-1).getName() + " " + employees.get(employees.size()-1).getSex() + ".";
+//            return result;
+//        }
+    @DeleteMapping("/{id}")
+    public  @ResponseBody String getAdd(@PathVariable("id") int id) {
+        List<Employee> employees = getEmployeeList();
+        List<Employee>newList=employees.stream().filter(item->item.getEmployeeID()==id).collect(Collectors.toList());
+        String result="";
+        for (int i = 0; i < newList.size() - 1; i++) {
+            result += employees.get(i).getEmployeeID() + " " + employees.get(i).getName() + " " + employees.get(i).getSex() + ",";
+        }
+        result += employees.get(employees.size()-1).getEmployeeID() + " " + employees.get(employees.size()-1).getName() + " " + employees.get(employees.size()-1).getSex() + ".";
+        return result;
+    }
+
+
+
+}
+
